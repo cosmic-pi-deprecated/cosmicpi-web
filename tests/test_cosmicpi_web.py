87cf49db65c18a7a0c4bba4ae689d46b02505e7a
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-12-07 20:44:11 +0100

    Update tests

--- tests/test_cosmicpi_web.py
+++ tests/test_cosmicpi_web.py
@@ -35,7 +35,7 @@ def test_command_line_interface():
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert 'cosmicpi_web.cli.main' in result.output
+    assert 'run' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
